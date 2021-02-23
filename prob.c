458d6ab5b85b20abc645815dd4ad7830bb5dc9b0
Panagiotis Palamidas <palas.pan.23@gmail.com>
2021-02-23 14:26:04 +0200

    Create prob.c

---
+++ prob.c
@@ -0,0 +1,13 @@
+// PROB ALGORITHM
+#include <stdio.h>
+#include <stdlib.h>
+#include "prob.h"
+
+
+void prob(int dtnumb,int fsemi,int *fnc){
+  if((dtnumb+1)>fsemi)
+  {
+    *fnc=1;
+    printf("Please color the map first\n");
+  }
+}
